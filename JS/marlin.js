9e5d29a905447ae36fdb580812ddd3cb246ad362
RobertoPeredo <robertopgzm@gmail.com>
2022-05-15 14:04:58 -0500

    dinamic-states

--- JS/marlin.js
+++ JS/marlin.js
@@ -1,3 +1,197 @@
+let datosApi={
+  status: true,
+  message: "",
+  data: [
+      {
+          id_beach: 1,
+          beach_name: "Playa de Tijuana",
+          state: "BAJA CALIFORNIA",
+          city: "TIJUANA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 2,
+          beach_name: "Playa Coromuel, El tecolote",
+          state: "BAJA CALIFORNIA SUR",
+          city: "LA PAZ",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 3,
+          beach_name: "Playa Manigua, Playa Norte 1",
+          state: "CAMPECHE",
+          city: "CIUDAD DEL CARMEN",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 4,
+          beach_name: "Playa Boca del cielo",
+          state: "CHIAPAS",
+          city: "TONALÁ",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 5,
+          beach_name: "Cuyutlan, El Paraiso",
+          state: "COLIMA",
+          city: "ARMERÍA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 6,
+          beach_name: "Caleta, La Roqueta",
+          state: "GUERRERO",
+          city: "ACAPULCO",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 7,
+          beach_name: "Playa Yelapa, Playa Quimixto",
+          state: "JALISCO",
+          city: "PUERTO VALLARTA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 8,
+          beach_name: "Playa Nexpa, Playa Maruata",
+          state: "MICHOACAN",
+          city: "AQUILA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 9,
+          beach_name: "Playa Nuevo Vallarta 1, Playa Bucerias",
+          state: "NAYARIT",
+          city: "BAHÍA DE BANDERAS",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 10,
+          beach_name: "Playa Maguey, Playa Bahia Santa Cruz",
+          state: "OAXACA",
+          city: "HUATULCO",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 11,
+          beach_name: "Playa Puerto Morelos, Playa Delfines",
+          state: "QUINTA ROO",
+          city: "CANCÚN",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 12,
+          beach_name: "Playa Altada",
+          state: "SINALOA",
+          city: "BAHÍA DE ALTATA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 13,
+          beach_name: "Playa Bahia de Kino 1 ",
+          state: "SONORA",
+          city: "BAHÍA DE KINO",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 14,
+          beach_name: "Playa Sánchez Magallanes",
+          state: "TABASCO",
+          city: "CARDENAS",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 15,
+          beach_name: "Playa Barra del Tordo",
+          state: "TAMAULIPAS",
+          city: "ALDAMA",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 16,
+          beach_name: "Playa Tecolutla, Playa Noray",
+          state: "VERACRUZ",
+          city: "MARTÍNEZ DE LA TORRE",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      },
+      {
+          id_beach: 17,
+          beach_name: "Playa Telchac",
+          state: "YUCATAN",
+          city: "TELCHAC",
+          statistics: {
+              contamination_level: "Media",
+              quality: "Apta",
+              year: 2022
+          }
+      }
+  ]
+}
+
 mandarform=()=>{
     let form = document.forms["formulario"]
     let name= form.name.value;
@@ -127,119 +321,136 @@ async function postData(url = '', data = {}) {
                     //1
                     case "MEX2706":       
                      nombre="Baja California"; 
-                     guardar_localstorage(nombre); 
+                     id=1;
+                     guardar_localstorage(nombre,id); 
                      document.location.href="./info.html";
                     break;
 
                     //2
                     case "MEX2707":          
                     nombre="Baja california Sur"; 
-                     guardar_localstorage(nombre);  
+                    id=2;
+                     guardar_localstorage(nombre,id);                       
                      document.location.href="./info.html";                                      
                     break;
 
                     //3
                     case "MEX2711":          
                     nombre="Sinaloa"; 
-                     guardar_localstorage(nombre);  
+                    id=12;
+                    guardar_localstorage(nombre,id); 
                      document.location.href="./info.html";                                      
                     break;
 
                     //4
                     case "MEX2712":          
                     nombre="Sonora"; 
-                     guardar_localstorage(nombre);  
+                    id=13;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //5
                     case "MEX2716":          
                     nombre="Tamaulipas"; 
-                     guardar_localstorage(nombre);  
+                    id=15;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //6
                     case "MEX2718":          
                     nombre="Colima"; 
-                     guardar_localstorage(nombre);  
+                    id=5;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //7
                     case "MEX2719":          
                     nombre="Jalisco"; 
-                     guardar_localstorage(nombre);  
+                    id=7;
+                    guardar_localstorage(nombre,id); 
                      document.location.href="./info.html";                                      
                     break;
 
                     //8
                     case "MEX2720":          
                     nombre="Michoacán"; 
-                     guardar_localstorage(nombre);  
+                    id=8;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //9
                     case "MEX2721":          
                     nombre="Nayarit"; 
-                     guardar_localstorage(nombre);  
+                    id=9;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //10
                     case "MEX2722":          
                     nombre="Campeche"; 
-                     guardar_localstorage(nombre);  
+                    id=3;
+                    guardar_localstorage(nombre,id); 
                      document.location.href="./info.html";                                      
                     break;
 
                     //11
                     case "MEX2723":          
                     nombre="Oaxaca"; 
-                     guardar_localstorage(nombre);  
+                    id=10;
+                    guardar_localstorage(nombre,id); 
                      document.location.href="./info.html";                                      
                     break;
 
                     //12
                     case "MEX2725":          
                     nombre="Tabasco"; 
-                     guardar_localstorage(nombre);  
+                    id=14;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //13
                     case "MEX2729":          
                     nombre="Guerrero"; 
-                     guardar_localstorage(nombre);  
+                    id=6;
+                    guardar_localstorage(nombre,id);   
                      document.location.href="./info.html";                                      
                     break;
 
                     //14
                     case "MEX2734":          
                     nombre="Veracruz"; 
-                     guardar_localstorage(nombre);  
+                    id=16;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //15
                     case "MEX2735":          
                     nombre="Chiapas"; 
-                     guardar_localstorage(nombre);  
+                    id=4;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //16
                     case "MEX2736":          
                     nombre="Quintana Roo"; 
-                     guardar_localstorage(nombre);  
+                    id=11;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
                     //17
                     case "MEX2737":          
                     nombre="Yucatán"; 
-                     guardar_localstorage(nombre);  
+                    id=17;
+                    guardar_localstorage(nombre,id);  
                      document.location.href="./info.html";                                      
                     break;
 
@@ -251,21 +462,30 @@ async function postData(url = '', data = {}) {
 
 }
 
-function guardar_localstorage(nombre){
+function guardar_localstorage(nombre,id){
     localStorage.setItem("nombre",nombre)
+    localStorage.setItem("id",id)
+    
 
  }
 
  function escribir_nombre(){
     const state=document.getElementById("estado");
-    
     estado= localStorage.getItem("nombre");
+    i=localStorage.getItem("id");
     state.innerHTML= estado;
+    console.log(datosApi.data[i-1].beach_name);
+    _setnombrePlaya(datosApi.data[i-1].beach_name);
+_setMunicipio(datosApi.data[i-1].city)
+_setNivel(datosApi.data[i-1].statistics.contamination_level)
+_setStatus(datosApi.data[i-1].statistics.quality);
+
+
  }
 
  const _setnombrePlaya=(a)=>{
     const playa1=document.getElementById("playa1");// para  cambiar html
-    municipio1.innerHTML=a ;
+    playa1.innerHTML=a ;
 }
 const _setMunicipio=(a)=>{
     const municipio1=document.getElementById("municipio1");//para  cambiar html
@@ -275,7 +495,7 @@ const _setNivel=(a)=>{
     const nivel1=document.getElementById("nivel1");// para  cambiar html
     nivel1.innerHTML=a ;
 }
- const _setEstado=(a)=>{
+ const _setStatus=(a)=>{
     const status1=document.getElementById("status1");// para  cambiar html
     status1.innerHTML=a ;
 }
